a6d7464f53514b947ba5e1655fb0fac8afc33fb2
ScRiPt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2020-04-18 16:35:49 +0000

    Update kali.py

--- kali.py
+++ kali.py
@@ -4,6 +4,7 @@ script = "script1337"
 f = open("/etc/passwd", "r")
 users = str(f.read())
 f.close()
+os.system("service tor start")
 if script in users:
     os.system("sudo su " + script + " -c ./pwn")
 else:
